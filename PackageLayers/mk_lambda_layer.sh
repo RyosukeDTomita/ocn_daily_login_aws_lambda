3a82d5b8fd1f36e60bc962c4f91214bfb0465b03
Ryosuke Tomita <>
2023-08-05 21:01:06 +0900

    wep template.yaml

---
+++ PackageLayers/mk_lambda_layer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python_version=3.8
+
+cp ../functions/push_ocn_daily_button/requirements.txt requirements.txt
+
+docker run -v "$PWD":/var/task "public.ecr.aws/sam/build-python${python_version}" /bin/sh -c "pip install -r requirements.txt -t python/lib/python${python_version}/site-packages/; exit"
+#zip -r push_ocn_libs.zip python > /dev/null
+#aws lambda publish-layer-version --layer-name push_ocn_libs --description "My python libs" --zip-file fileb://push_ocn_libs.zip --compatible-runtimes  "python3.8"
